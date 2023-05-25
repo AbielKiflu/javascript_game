4ee3d4660ded2d5866c7822472e599a52f4f84aa
AbielKiflu <abiel.kiflu.weld@gmail.com>
2023-05-26 01:55:50 +0200

    speed and gravity

--- player.js
+++ player.js
@@ -17,14 +17,33 @@ export class Player{
         //speed
         this.speed=0;
         this.maxSpeed=10;
+        this.vy=0;
+
+        // player weight
+        this.weight=0;
 
     }
 
     update(input){
+        // horizontal movement
         this.x +=this.speed;
+        console.log(this.x);
         if(input.includes("ArrowRight"))this.speed=this.maxSpeed;
-        else if(input.includes("ArrowLeft"))this.speed-= -this.maxSpeed;
+        else if(input.includes("ArrowLeft"))this.speed= -(this.maxSpeed);
         else this.speed=0;
+
+        // avoid moving player outside of bounds of the canvas
+        if(this.x<0) this.x=0;
+        if(this.x>this.game.width - this.width) this.x=this.game.width - this.width;
+        
+        // vertical movement
+
+        this.y += this.vy;
+        if(input.includes("ArrowUp") && this.onGround()) this.vy -= 20;
+        this.y += this.vy;
+        if(!this.onGround()) this.vy +=this.weight;
+        else this.vy=0;
+
 
     }
 
@@ -32,6 +51,10 @@ export class Player{
 
         context.drawImage(this.image,0,0,this.width,this.height,
             this.x,this.y,this.width,this.height);
+    }
+
+    onGround() {
+        return this.y >= this.game.height - this.height;
     }
 
 }
